995c85ba4d1195ad1277059ac5a187e2e0157705
ah-OOG-ah <75745146+ah-OOG-ah@users.noreply.github.com>
2023-10-05 15:23:03 -0400

    Add license headers with spotless

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     id("java")
+    id("com.diffplug.spotless") version "6.22.0"
 }
 
 group = "klaxon.klaxon"
@@ -18,6 +19,35 @@ dependencies {
 
     implementation("org.jzy3d:jzy3d-core:2.2.1")
     implementation("org.jzy3d:jzy3d-native-jogl-swing:2.2.1")
+}
+
+spotless {
+
+    java {
+        // don't need to set target, it is inferred from java
+
+        // make sure every file has the following copyright header.
+        // optionally, Spotless can set copyright years by digging
+        // through git history (see "license" section below)
+        licenseHeader(
+            "/** LazerBeam - a program to calculate beam deflection and possibly a bit more\n" +
+            " * Copyright (C) \$YEAR  ah-OOG-ah\n" +
+            " * \n" +
+            " * This program is free software: you can redistribute it and/or modify\n" +
+            " * it under the terms of the GNU General Public License as published by\n" +
+            " * the Free Software Foundation, either version 3 of the License, or\n" +
+            " * (at your option) any later version.\n" +
+            " * \n" +
+            " * This program is distributed in the hope that it will be useful,\n" +
+            " * but WITHOUT ANY WARRANTY; without even the implied warranty of\n" +
+            " * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the\n" +
+            " * GNU General Public License for more details.\n" +
+            " * \n" +
+            " * You should have received a copy of the GNU General Public License\n" +
+            " * along with this program.  If not, see <https://www.gnu.org/licenses/>.\n" +
+            " */\n"
+        )
+    }
 }
 
 tasks.test {
--- src/main/java/klaxon/klaxon/lazerbeam/Graph.java
+++ src/main/java/klaxon/klaxon/lazerbeam/Graph.java
@@ -1,3 +1,20 @@
+/** LazerBeam - a program to calculate beam deflection and possibly a bit more
+ * Copyright (C) 2023  ah-OOG-ah
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 package klaxon.klaxon.lazerbeam;
 
 import org.jzy3d.analysis.AbstractAnalysis;
